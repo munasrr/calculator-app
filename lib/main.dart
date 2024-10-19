599bcf8ec3099e573c48780e3cf567c03ae2e8a1
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-10-20 00:30:17 +0300

    heigth

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _CalculatorState extends State<Calculator> {
       children: <Widget>[
         // Display for output
         const SizedBox(
-          height: 19,
+          height: 21,
         ),
         Container(
           color: Colors.green,
